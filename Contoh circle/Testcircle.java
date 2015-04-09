8d21ced2299f03c452cc7f69aba334296640998c
hassanfikri <nizam_mulachela@yahoo.com>
2015-04-09 22:50:28 +0700

    Revert "Revert "Tugas 3""
    This reverts commit 851345d5cbf6da5cdb64b5b043d1ab6a4050af27.

---
+++ Contoh circle/Testcircle.java
@@ -0,0 +1,16 @@
+public class  Testcircle{
+
+	public static void main(String[] args) {
+	
+	circle cahya = new circle();
+	circle ardi = new	circle (30);
+	circle akbar = new circle (60,"AbangGalau");
+
+
+	System.out.println("radius = "+ cahya.radius +"color = "+ cahya.color + " luas");	
+	System.out.println("radius = "+ ardi.radius +"color = "+ ardi.color + " luas");	
+	System.out.println("radius = "+ akbar.radius +"color = "+ akbar.color + " luas");	
+
+	}
+	
+}
---
+++ Contoh circle/circle.java
@@ -0,0 +1,42 @@
+public class  circle{
+	
+	 double radius;
+	 String color;
+
+		public circle(){
+
+			radius = 1.0;
+			color = "red";
+
+		}
+
+		public circle(double r){ 
+
+			radius = r;
+			color = "red";
+
+		}
+		public circle(double r, String c){
+
+			radius = r;
+			color = c;
+		}
+
+		public double getRadius(){
+
+			return radius;
+
+		}
+		public String getColor(){
+
+			return color;
+
+		}
+
+		public double getArea(){
+
+			return radius*radius*Math.PI;
+		}
+
+	
+}
